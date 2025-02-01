1e6f15d9c9024fab6825f68710324f65be78088e
bidisha979 <bidishasarka979@gmail.com>
2025-02-01 20:49:51 +0530

    Updated routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://bidisha:0420@bidishaproject.rgyjmyb.mongodb.net/?retryWrites=true&w=majority&appName=bidishaproject")
+mongoose.connect("mongodb+srv://bidisha:0420@bidishaproject@bidishaproject.rgyjmyb.mongodb.net/portfoliodb?retryWrites=true&w=majority")
   .then(() => console.log("Connected to MongoDB ✅"))
   .catch((err) => console.error("MongoDB connection error ❌", err));
 
